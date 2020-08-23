b152f088fe98b8557fab540c8c153aa312e5407e
Wallace <wbescrovaine@gmail.com>
2020-08-23 05:41:29 -0300

    call data exemple

---
+++ db.js
@@ -0,0 +1,23 @@
+// import mariadb
+var mariadb = require('mariadb');
+
+// create a new connection pool
+const pool = mariadb.createPool({
+    host: "localhost",
+    user: "pi",
+    password: "123",
+    database: "baba_eletronica"
+});
+
+// expose the ability to create new connections
+module.exports = {
+    getConnection: function () {
+        return new Promise(function (resolve, reject) {
+            pool.getConnection().then(function (connection) {
+                resolve(connection);
+            }).catch(function (error) {
+                reject(error);
+            });
+        });
+    }
+} 
--- server.js
+++ server.js
@@ -1,44 +1,80 @@
-const mariadb = require('mariadb');
 const express = require('express');
 const bodyParser = require('body-parser');
+const pool = require('./db');
 
 // --------------------------------------------------------------
 // Connection Mariadb
-let con = mariadb.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "123",
-    database: "baba_eletronica",
-});
+// let con = mariadb.createConnection({
+//     host: "192.168.100.33",
+//     user: "root",
+//     password: "123",
+//     database: "baba_eletronica",
+// });
 
-con.connect((err) => {
-    if (err) {
-        throw err;
-    } else {
-        setTimeout(() => { console.log("Waiting 2sec"), 2000 });
-        console.log('We are connected with DataBase');
-    }
-});
+// con.connect(function (err) {
+//     if (err) {
+//         throw err;
+//     } else {
+//         setTimeout(() => { console.log("Waiting 2sec"), 2000 });
+//         console.log('We are connected with DataBase');
+//     }
+// });
+
+// con.getConnection();
+
+
+// pool.getConnection()
+//     .then(() => {
+//         console.log("conectei");
+
+
+
+//     }).catch((err) => {
+//         throw err;
+//     });
+
 // --------------------------------------------------------------
 // Express configure
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static('.'));
-app.listen(8080, () => {
+// app.use(express.static('/home/pi/Electronic-Nanny-Server'));
+app.listen(8080, function () {
     setTimeout(() => { console.log("Waiting 2sec"), 2000 });
     console.log('Listening on port 8080');
 });
 // --------------------------------------------------------------
 // Get cry report
 
-app.get('/ServerRequest/CryReport', function (req, res) {
-    let sql = `SELECT * FROM monitoramento_choro`;
+// app.get('/ServerRequest/CryReport', function (req, res) {
+//     let sql = `SELECT * FROM monitoramento_choro`;
 
-    con.query(sql, function (err, result, fields) {
-        if (err) {
-            throw err;
-        }
-        console.log(result);
-        return res.json(result);
-    });
+//     con.query(sql, function (err, result, fields) {
+//         if (err) {
+//             throw err;
+//         }
+//         console.log(result);
+//         return res.json(result);
+//     });
+// });
+
+app.get('/ServerRequest/CryReport', async (req, res) => {
+    let conn;
+    try {
+        // establish a connection to MariaDB
+        conn = await pool.getConnection();
+
+        // create a new query
+        var query = `SELECT * FROM monitoramento_choro`;
+
+        // execute the query and set the result to a new variable
+        var rows = await conn.query(query);
+
+        // return the results
+        res.send(rows);
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) return conn.release();
+    }
 });
