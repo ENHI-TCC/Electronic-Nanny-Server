22c0427c04aca71e7c94229a9f16b052bcde9057
Wallace <wbescrovaine@gmail.com>
2020-09-05 20:42:17 -0300

    Add Delete All and Insert

--- src/controllers/BabyMonitorController.js
+++ src/controllers/BabyMonitorController.js
@@ -1,4 +1,5 @@
 const pool = require('../database/db');
+const moment = require('moment');
 
 module.exports = {
     async index(req, res) {
@@ -22,6 +23,58 @@ module.exports = {
         }
     },
 
+    async insertNewCry(req, res) {
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            var values = [];
+            // var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+            var mysqlTimestamp = new Date();
+            mysqlTimestamp.setHours(mysqlTimestamp.getHours() - 3);
+            // var mysqlTimestamp = moment().utc().format('YYYY-MM-DD HH:mm:ss');
+
+            values.push(mysqlTimestamp);
+
+            console.log(mysqlTimestamp);
+            console.log(values);
+
+            var query = "INSERT INTO monitoramento_choro (data)  VALUES ?";
+
+            await conn.query(query, [values], function (error, results, fields) {
+                if (error) throw error;
+                res.send("Insert complete");
+            });
+            res.send("Insert complete");
+
+        } catch (err) {
+            throw err;
+        } finally {
+            if (conn) return conn.release();
+        }
+    },
+    async deleteAll(req, res) {
+        let conn;
+        try {
+            // establish a connection to MariaDB
+            conn = await pool.getConnection();
+
+            // create a new query
+            var query = 'DELETE FROM `monitoramento_choro` ';
+
+            await conn.query(query, function (error, results, fields) {
+                if (error) throw error;
+                res.send('Delete All Complete!');
+            });
+            res.send('Delete All Complete!');
+
+        } catch (err) {
+            throw err;
+        } finally {
+            if (conn) return conn.release();
+        }
+
+    },
+
     async deleteById(req, res) {
         let conn;
         try {
@@ -33,7 +86,7 @@ module.exports = {
 
             await conn.query(query, [req.body.id], function (error, results, fields) {
                 if (error) throw error;
-                res.end('Record has been deleted!');
+                res.send('Record has been deleted!');
             });
             res.send("Deleted");
 
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ const routes = express.Router();
 
 // Rotas Baby Monitor
 routes.get('/ServerRequest/CryReport', BabyMonitorController.index);
+routes.post('/ServerRequest/NewCry', BabyMonitorController.insertNewCry);
 routes.post('/ServerRequest/DeleteCryById', BabyMonitorController.deleteById);
+routes.delete('/ServerRequest/DeleteAll', BabyMonitorController.deleteAll);
 
 module.exports = routes;
